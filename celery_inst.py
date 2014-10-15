d218152189fc3af1761930e1bbe54eca29562023
jerkos <cram@hotmail.fr>
2014-10-15 20:43:46 +0200

    test

--- celery_inst.py
+++ celery_inst.py
@@ -19,10 +19,11 @@ from mzos.results_exporter import ResultsExporter
 CLOUDAMQP_URL = os.environ.get('CLOUDAMQP_URL', 'amqp://')
 
 celery = Celery('celery_inst',
-                broker=CLOUDAMQP_URL,
+                broker=CLOUDAMQP_URL + '?heartbeat=30',
                 backend='amqp')
 
 celery.BROKER_POOL_LIMIT = 1
+celery.BROKER_CONNECTION_TIMEOUT = 10
 
 # Optional configuration, see the application user guide.
 celery.conf.update(
