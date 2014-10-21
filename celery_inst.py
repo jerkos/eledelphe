77609735e09251f11cd97e137ecb519ced28d97d
jerkos <cram@hotmail.fr>
2014-10-21 16:09:26 +0200

    fix some stuffs

--- celery_inst.py
+++ celery_inst.py
@@ -10,11 +10,11 @@ CLOUDAMQP_URL = os.environ.get('CLOUDAMQP_URL', 'amqp://')
 
 celery = Celery('celery_inst',
                 #backend='amqp',
-                broker=CLOUDAMQP_URL,
+                broker=CLOUDAMQP_URL + '?heartbeat=30',
                 include=['tasks'])
 
 celery.BROKER_POOL_LIMIT = 1
-# celery.BROKER_CONNECTION_TIMEOUT = 10
+celery.BROKER_CONNECTION_TIMEOUT = 10
 
 # Optional configuration, see the application user guide.
 celery.conf.update(
